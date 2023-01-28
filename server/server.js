24167f4d84bcaf1888df1303c98134433bd5c80a
erMisetto <123638955+erMisetto@users.noreply.github.com>
2023-01-28 11:54:01 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import {Configuration, OpenAIApi} from 'openai';
 
 dotenv.config()
 
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -15,6 +16,7 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 
 app.get('/', async (req, res) => {
   res.status(200).send({
